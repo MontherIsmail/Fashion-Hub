a645af457aebbcdb22965ac0a480619669b7d78b
=DeenaAlghazali <deenaalghazali11@gmail.com>
2022-04-03 23:47:56 +0300

    create login component

--- client/src/Components/Login.jsx
+++ client/src/Components/Login.jsx
@@ -1,34 +1,31 @@
-import { Component } from "react";
-
-class Login extends Component {
-  render() {
-    const { handleChange, handleSubmit } = this.props;
-    return (
-      <form onSubmit={handleSubmit}>
-        <label>
-          name
-          <input
-            type="text"
-            placeholder="Enter your name ..."
-            id="name"
-            onChange={handleChange}
-          />
-        </label>
-        <br />
-        <label>
-          password
-          <input
-            type="password"
-            placeholder="your password ..."
-            id="password"
-            onChange={handleChange}
-          />
-        </label>
-        <br />
-        <button>Login</button>
-      </form>
-    );
-  }
-}
+const Login = ({ handleChange, handleSubmit, loged }) => {
+  return loged ? (
+    <h1>you are loged in</h1>
+  ) : (
+    <form onSubmit={handleSubmit}>
+      <label>
+        name
+        <input
+          type="text"
+          placeholder="Enter your name ..."
+          id="name"
+          onChange={handleChange}
+        />
+      </label>
+      <br />
+      <label>
+        password
+        <input
+          type="password"
+          placeholder="your password ..."
+          id="password"
+          onChange={handleChange}
+        />
+      </label>
+      <br />
+      <button>Login</button>
+    </form>
+  );
+};
 
 export default Login;
