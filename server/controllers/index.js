b52a05f6856618f4abb63f5ca9485f1275c9f936
=DeenaAlghazali <deenaalghazali11@gmail.com>
2022-04-04 01:42:45 +0300

    create get one page controller

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -2,10 +2,12 @@ const { addProduct } = require('./addProduct');
 const getAllProducts = require('./getAllProducts');
 const removeProduct = require('./removeProduct');
 const updateProductHandler = require('./updateProductHandler');
+const getOneProduct = require('./getOneProduct');
 
 module.exports = {
   addProduct,
   getAllProducts,
   removeProduct,
   updateProductHandler,
+  getOneProduct,
 };
