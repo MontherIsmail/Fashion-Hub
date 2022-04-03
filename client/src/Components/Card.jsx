6ed50e8a07d3f784b5644c95d1ba9b451041a059
=DeenaAlghazali <deenaalghazali11@gmail.com>
2022-04-03 17:31:20 +0300

    create Card componnent

--- client/src/Components/Card.jsx
+++ client/src/Components/Card.jsx
@@ -1,19 +1,23 @@
 import React, { Component } from "react";
-
+import { Link } from "react-router-dom";
 class Card extends Component {
   render() {
     const { product, deleteItem } = this.props;
     return (
-      <div>
-        <span>{product.name}</span>
-        <span>{product.category}</span>
-        <span>{product.quantity}</span>
-        <span>{product.prev_price}</span>
-        <span>{product.new_price}</span>
-        <button className="delete" onClick={() => deleteItem(product.id)}>
-          delete
-        </button>
-        <button>edit</button>
+      <div className="some-cards">
+        <div className="cards">
+          <Link to={`/product/${product.id}`}>
+            <span>{product.name}</span>
+          </Link>
+          <span>{product.category}</span>
+          <span>{product.quantity}</span>
+          <span>{product.prev_price}</span>
+          <span>{product.new_price}</span>
+          <button className="delete" onClick={() => deleteItem(product.id)}>
+            delete
+          </button>
+          <button>edit</button>
+        </div>
       </div>
     );
   }
