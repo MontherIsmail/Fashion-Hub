d8869ffe6b7be1fae9de9885f641523579416af1
=DeenaAlghazali <deenaalghazali11@gmail.com>
2022-04-03 18:05:56 +0300

    fix app.js (front)

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,29 +1,29 @@
 import { Component } from "react";
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
 import AllProducts from "./Components/AllProducts";
 import Nav from "./Components/Nav";
+import axios from "axios";
 
 class App extends Component {
   state = {
     products: [],
   };
   componentDidMount() {
-    fetch("http://localhost:3001/api/v1/products")
-      .then((res) => res.json())
-      .then((products) => this.setState({ products }));
+    axios
+      .get("/api/v1/products")
+      .then((res) => this.setState({ products: res.data }))
+      .catch((err) => console.log(err));
   }
+
   deleteItem = (id) => {
-    fetch(`http://localhost:3001/api/v1/products/${id}`, {
-      method: "DELETE",
-    })
+    axios
+      .delete(`/api/v1/products/${id}`)
       .then(() => {
         let products = this.state.products.filter((item) => item.id !== id);
         this.setState({ products });
       })
-      .catch((error) => {
-        console.log(error);
-      });
+      .catch((err) => console.log(err));
   };
   render() {
     const { products } = this.state;
