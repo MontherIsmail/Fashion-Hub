06b4930ceeb0ec7cffdce6c15b911890e96d3c95
Mohammed-Hamada <mohammed3rbio@gmail.com>
2022-04-04 17:00:57 +0300

    Add product_image column to database Relates #23

--- server/database/queries/addProductDB.js
+++ server/database/queries/addProductDB.js
@@ -1,6 +1,6 @@
 const connection = require('../config/connection');
 
 // eslint-disable-next-line camelcase
-const addProductDB = (name, category, prev_price, new_price, quantity) => connection.query('INSERT INTO products (name, category, prev_price,new_price, quantity) VALUES ($1, $2, $3, $4, $5) RETURNING *', [name, category, prev_price, new_price, quantity]);
+const addProductDB = (name, category, prev_price, new_price, quantity, product_image) => connection.query('INSERT INTO products (name, category, prev_price,new_price, quantity, product_image) VALUES ($1, $2, $3, $4, $5, $6) RETURNING *', [name, category, prev_price, new_price, quantity, product_image]);
 
 module.exports = addProductDB;
