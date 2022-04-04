3ebc7ed52791630a0e36e8b8d614e8893cfae21d
Mohammed-Hamada <mohammed3rbio@gmail.com>
2022-04-04 17:05:39 +0300

    Edit Validation Schema Relates #23

--- server/validation/addProductschema.js
+++ server/validation/addProductschema.js
@@ -1,11 +1,12 @@
 const joi = require('joi');
 
 const addProductSchema = joi.object({
-  name: joi.string().alphanum().required(),
-  category: joi.required(),
-  quantity: joi.required(),
-  prev_price: joi.number().min(1).max(5).required(),
-  new_price: joi.number().min(1).max(5).required(),
+  name: joi.string().required(),
+  category: joi.string().required(),
+  quantity: joi.number().required(),
+  prev_price: joi.number().min(1).max(90000).required(),
+  new_price: joi.number().min(1).max(90000).required(),
+  product_image: joi.string().required(),
 });
 
 module.exports = { addProductSchema };
