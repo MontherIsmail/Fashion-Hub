bad956db7bbaea1a3fc630505d5d85b9922ebc81
Mohammed-Hamada <mohammed3rbio@gmail.com>
2022-04-04 17:01:54 +0300

    Update controllers to accept product_image Relates #23

--- server/controllers/updateProductHandler.js
+++ server/controllers/updateProductHandler.js
@@ -4,8 +4,8 @@ const { customError } = require('../errors');
 module.exports = (req, res, next) => {
   const { id } = req.params;
   updateProduct(id, req.body)
-    .then(() => {
-      res.json({ message: 'product Updated' });
+    .then((data) => {
+      res.json({ status: 200, editedProduct: data.rows[0], message: 'product Updated' });
     })
     .catch((err) => {
       if (err.code) next(customError({ status: 400 }));
