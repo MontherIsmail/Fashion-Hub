38aad11185f0682e9202ff81f8c865b1f1e6cc29
unknown <montherismail90@gmail.com>
2022-04-05 00:49:50 +0300

    Creatte footer jsx css Relates #45

---
+++ client/src/Components/Footer.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import footerLogo from "../assets/footerLogo.PNG";
+import payment from "../assets/payment.PNG";
+
+function Footer() {
+  return (
+    <footer>
+      <img src={payment} alt="payment" />
+      <div class="logo-copyright">
+        <img src={footerLogo} alt="footer logo" />
+        <p>
+          © 2022 Fashion Hub. All rights reserved | Design by Monther Alzamli.
+        </p>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
