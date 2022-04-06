0c9b9e16ea4b3147b17efbe8192be6c445f50568
Mohammed-Hamada <mohammed3rbio@gmail.com>
2022-04-07 01:30:25 +0300

    commit

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ const { join } = require('path');
 const express = require('express');
 const morgan = require('morgan');
 const cors = require('cors');
-const router = require('./routes/router');
 
 const app = express();
 const {
@@ -17,7 +16,7 @@ app.use(cors());
 app.use(express.urlencoded({ extended: false }));
 app.use('/api/v1', router);
 app.use(express.static(join(__dirname, 'client', 'build')));
-router.get('*', (req, res) => {
+app.use('*', (req, res) => {
   res.sendFile(join(__dirname, 'client', 'build', 'index.html'));
 });
 
